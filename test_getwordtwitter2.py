7f62e98b800102e22e8468094c5bd4e8e13d2fc3
jinyu-tn <55299831+jinyu-tn@users.noreply.github.com>
2020-02-28 18:17:04 -0500

    Add files via upload

---
+++ test_getwordtwitter2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 28 17:53:59 2020
+
+@author: taimaame
+"""
+
+
+from getwordtwitter2 import get_all_tweets
+
+def test_get_all_tweets():
+    assert get_all_tweets() != []
