b14828dddcd972a5a333cc87d8a1cd7a219d9ec3
jinyu-tn <55299831+jinyu-tn@users.noreply.github.com>
2020-02-28 17:08:07 -0500

    Add files via upload

---
+++ test_imagestovideo.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 28 16:32:55 2020
+
+@author: taimaame
+"""
+
+
+from imagestovideo import imagetovideo
+
+def test_imagetovideo():
+    assert imagetovideo() == "Pass!"
